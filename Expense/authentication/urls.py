51f989d56ced3158153332c55f94fbe290daf786
Bismark Kadogbe <neroaboagye@gmail.com>
2022-12-10 18:40:50 +0000

    Done with registration activation

--- Expense/authentication/urls.py
+++ Expense/authentication/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import (RegistrationView,UserValidationView,EmailValidationView)
+from .views import (RegistrationView,UserValidationView,EmailValidationView,VerificationView,LoginView)
 from django.views.decorators.csrf import csrf_exempt
 
 
 
 urlpatterns = [
-    path("register/",RegistrationView.as_view(), name='register'),
+    path("register",RegistrationView.as_view(), name='register'),
+    path("login",LoginView.as_view(), name='login'),
     path("validate-username",csrf_exempt(UserValidationView.as_view()), name='validate-username'),
     path("validate-email",csrf_exempt(EmailValidationView.as_view()), name='validate-email'),
+    path("activate/<uidb64>/<token>",csrf_exempt(VerificationView.as_view()), name='activate'),
 ]
--- Expense/authentication/views.py
+++ Expense/authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 import json
 from django.http import JsonResponse
@@ -6,6 +6,13 @@ from django.contrib.auth.models import User
 from validate_email import validate_email
 from django.contrib import messages
 from django.core.mail import EmailMessage
+from .utils import token_generator
+
+
+from django.urls import reverse
+from django.utils.encoding import force_bytes, force_str , DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
 
 
 # Create your views here.
@@ -70,8 +77,15 @@ class RegistrationView(View):
                 # This is to disable user account
                 user.is_active =False
                 user.save()
+
+                uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+
+                domain = get_current_site(request).domain
+                link = reverse('activate', kwargs={'uidb64':uidb64,'token':token_generator.make_token(user)})
+
+                activate_url = 'http://'+domain+link
                 email_subject = "Activate your account."
-                email_body = 'Test Body'
+                email_body = "Hi "+ user.username + ", Please use this link to verify your account 😊.\n" + activate_url
                 email = EmailMessage(
                     email_subject,
                     email_body,
@@ -83,5 +97,31 @@ class RegistrationView(View):
                 return render(request,'authentication/register.html')
 
         return render(request,'authentication/register.html')
+
+
+class VerificationView(View):
+    def get(self,request,uidb64,token):
+        try:
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=id)
+
+            if not token_generator.check_token(user,token):
+                return redirect("login"+"?message="+"User already activated😑.")
+
+            if user.is_active:
+                return redirect('login')
+            user.is_active=True
+            user.save()
+            return redirect('login')
+
+            messages.success(request,"Account activated successfully😊.")
+        except Exception as ex:
+            pass
+        return redirect('login')
+
+
+class LoginView(View):
+    def get(self,request):
+        return render(request,'authentication/login.html')
 
         
