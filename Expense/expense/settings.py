2623f48113aabcc28f55e1c7d60d559dc614de2a
Bismark Kadogbe <neroaboagye@gmail.com>
2022-12-19 22:53:09 +0000

    Added Income App

--- Expense/expense/settings.py
+++ Expense/expense/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'expenses_app',
     'userpreferences',
+    'user_income',
 ]
 
 MIDDLEWARE = [
---
+++ Expense/user_income/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+# Create your models here.
+
+class UserIncome(models.Model):
+    amount = models.FloatField()
+    date = models.DateField(default=now)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    source=models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.source
+
+    
+    class Meta:
+        # This sort the output
+        ordering: ('-date')
+
+
+class Source(models.Model):
+    name= models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.name
+
+
