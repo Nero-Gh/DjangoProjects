092f6d793e85975db5905661903a6b75817d5442
Bismark Kadogbe <neroaboagye@gmail.com>
2022-12-20 01:03:42 +0000

    customizing  expense admin page

--- Expense/expenses_app/admin.py
+++ Expense/expenses_app/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Category,Expenses
 
 # Register your models here.
+#Customizing Expense admin Page
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount','description','owner','category','date',)
+    search_fields = ('description','owner','category','date',)
+    list_per_page = 5
 
-admin.site.register(Expenses)
+admin.site.register(Expenses,ExpenseAdmin)
 admin.site.register(Category)
 
 
